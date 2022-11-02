81c512d3bfaa58d06f469befa1b6380b2f12d2f4
John Edelbi <info@jedelbi.com>
2022-11-02 18:50:20 +0100

    Made some improvements to test text

--- template-write-up/sandbox.test.js
+++ template-write-up/sandbox.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const message = 'please enter a number';
 const addUp = (num) => {
-  if (num === '' || num === ' ' || num.isNaN) {
+  if (num === '' || num === ' ' || isNaN(num)) {
     return message;
   } else {
     let sum = 0;
@@ -13,29 +13,29 @@ const addUp = (num) => {
 };
 
 describe('Add up the Numbers from a Single Number ', () => {
-  it('Add up all the numbers from 1 to the number you passed to the function', () => {
+  it('Add up all the numbers from 1 to the number 4', () => {
     const num = 4;
     expect(addUp(num)).toEqual(10);
   });
-  it('Add up all the numbers from 1 ', () => {
+  it('Add up all the numbers from 1 to the number 13', () => {
     const num = 13;
     expect(addUp(num)).toEqual(91);
   });
-  it('Add up all the number from 1', () => {
+  it('Add up all the numbers from 1 to the number 600', () => {
     const num = 600;
     expect(addUp(num)).toEqual(180300);
   });
-  it('Add up all the numbers from', () => {
+  it('use empty string', () => {
     const num = '';
     expect(addUp(num)).toEqual(message);
   });
 
-  it('Add up all the numbers from', () => {
+  it('use empty space', () => {
     const num = ' ';
     expect(addUp(num)).toEqual(message);
   });
 
-  it('return confirm to user to enter a number', () => {
+  it('use letters', () => {
     const num = 'a';
     expect(addUp(num)).toEqual(message);
   });
