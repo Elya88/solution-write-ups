9f406fa93f63ff614d9b23636b340ae70f60ee99
John Edelbi <info@jedelbi.com>
2022-11-02 15:29:28 +0100

    finish the test

--- template-write-up/sandbox.test.js
+++ template-write-up/sandbox.test.js
@@ -1,13 +1,17 @@
-'use strict';
- function addUp(num) {
-   let sum = 0; 
-   for(let i = 1; i <= num; i++) {
-     sum += i;
-   }
-   
-   return sum;
- }
-console.log(addUp(''));
+function addUp(num) {
+
+  if ( num ==="" || num === " " || isNaN(num) ){
+    return "please enter a number";
+    
+  } else {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    return sum; 
+  }
+
+}
 
 describe("Add up the Numbers from a Single Number ", () => {
   it("Add up all the numbers from 1 to the number you passed to the function", () => {
@@ -22,18 +26,18 @@ describe("Add up the Numbers from a Single Number ", () => {
     let num = 600;
      expect(addUp(num)).toEqual(180300)
   })
- 
+  it("Add up all the numbers from", () => {
+    let num = "";
+       expect(addUp(num)).toEqual("please enter a number");
   })
 
-  describe("Add up the number from a single number ", () => {
-    it("return confirm to user to enter a number", () => {
-      let num = 1;
-        expect(addUp(num)).toEqual(1);
-    })
-    it("Add up all the numbers from", () => {
-      let num = 13;
-         expect(addUp(num)).toEqual(91);
-    })
-   })
+  it("Add up all the numbers from", () => {
+    let num = " ";
+       expect(addUp(num)).toEqual("please enter a number");
+  })
 
-
+  it("return confirm to user to enter a number", () => {
+    let num = "a";
+      expect(addUp(num)).toEqual("please enter a number");
+  })
+});
