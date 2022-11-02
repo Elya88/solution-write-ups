f490814a915f6eea7724556e4a6d5ff3f94e05d8
John Edelbi <info@jedelbi.com>
2022-11-02 18:36:01 +0100

    fix all eslint recommendation

--- template-write-up/sandbox.test.js
+++ template-write-up/sandbox.test.js
@@ -1,43 +1,42 @@
-function addUp(num) {
-
-  if ( num ==="" || num === " " || isNaN(num) ){
-    return "please enter a number";
-    
+/* eslint-disable linebreak-style */
+const message = 'please enter a number';
+const addUp = (num) => {
+  if (num === '' || num === ' ' || num.isNaN) {
+    return message;
   } else {
     let sum = 0;
     for (let i = 1; i <= num; i++) {
       sum += i;
     }
-    return sum; 
+    return sum;
   }
+};
 
-}
+describe('Add up the Numbers from a Single Number ', () => {
+  it('Add up all the numbers from 1 to the number you passed to the function', () => {
+    const num = 4;
+    expect(addUp(num)).toEqual(10);
+  });
+  it('Add up all the numbers from 1 ', () => {
+    const num = 13;
+    expect(addUp(num)).toEqual(91);
+  });
+  it('Add up all the number from 1', () => {
+    const num = 600;
+    expect(addUp(num)).toEqual(180300);
+  });
+  it('Add up all the numbers from', () => {
+    const num = '';
+    expect(addUp(num)).toEqual(message);
+  });
 
-describe("Add up the Numbers from a Single Number ", () => {
-  it("Add up all the numbers from 1 to the number you passed to the function", () => {
-    let num = 4;
-      expect(addUp(num)).toEqual(10);
-  })
-  it("Add up all the numbers from 1 ", () => {
-    let num = 13;
-       expect(addUp(num)).toEqual(91);
-  })
-  it("Add up all the number from 1", () => {
-    let num = 600;
-     expect(addUp(num)).toEqual(180300)
-  })
-  it("Add up all the numbers from", () => {
-    let num = "";
-       expect(addUp(num)).toEqual("please enter a number");
-  })
+  it('Add up all the numbers from', () => {
+    const num = ' ';
+    expect(addUp(num)).toEqual(message);
+  });
 
-  it("Add up all the numbers from", () => {
-    let num = " ";
-       expect(addUp(num)).toEqual("please enter a number");
-  })
-
-  it("return confirm to user to enter a number", () => {
-    let num = "a";
-      expect(addUp(num)).toEqual("please enter a number");
-  })
+  it('return confirm to user to enter a number', () => {
+    const num = 'a';
+    expect(addUp(num)).toEqual(message);
+  });
 });
